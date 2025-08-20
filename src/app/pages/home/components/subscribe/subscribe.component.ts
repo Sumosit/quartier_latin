2ae47efbf813d3d47a64998c5ae4d6385314e137
Yerlan <erlan99isaev@gmail.com>
2025-08-20 11:42:35 +0500

    asdasd

--- src/app/pages/home/components/subscribe/subscribe.component.ts
+++ src/app/pages/home/components/subscribe/subscribe.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {UiBtnTextIconComponent} from '../../../../shared/ui/ui-btn-text-icon/ui-btn-text-icon.component';
 import {UiBtnIconComponent} from '../../../../shared/ui/ui-btn-icon/ui-btn-icon.component';
+import {SvgIconComponent} from 'angular-svg-icon';
 
 @Component({
   selector: 'app-subscribe',
   imports: [
     UiBtnTextIconComponent,
-    UiBtnIconComponent
+    UiBtnIconComponent,
+    SvgIconComponent
   ],
   templateUrl: './subscribe.component.html',
   styleUrl: './subscribe.component.scss'
